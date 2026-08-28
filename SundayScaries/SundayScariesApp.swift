7cc6cfe5eac69e2dae05bb97630603118641474a
shermdiggity <cole.b.sherman@gmail.com>
2026-08-28 16:56:22 -0700

    Initial Commit

---
+++ SundayScaries/SundayScariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SundayScariesApp.swift
+//  SundayScaries
+//
+//  Created by Cole Sherman on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct SundayScariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
